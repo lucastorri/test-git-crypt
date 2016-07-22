e80fc56237b683ce516fbe69c55bcb16ea099315
Lucas Torri <lucastorri@gmail.com>
2016-07-22 12:26:01 +0200

    add a more powerfull rendering

--- render.sh
+++ render.sh
@@ -2,10 +2,31 @@
 
 set -ue
 
-source secretfile
+cd `dirname "$0"`
 
 render-template() {
-  eval "echo \"$(cat $1)\""
+  local template_file="$1"
+  eval "echo \"$(cat "$template_file")\""
 }
 
-render-template "$1"
+readonly system="$1"
+readonly environment="$2"
+
+readonly def_keys_file="configs/$system/default"
+readonly env_keys_file="configs/$system/$environment"
+
+readonly templates_source="templates/$system"
+
+readonly rendered_output="rendered/$system"
+
+mkdir -p "$rendered_output"
+
+source "$def_keys_file"
+source "$env_keys_file"
+
+for template in `find "$templates_source" -type f`; do
+  rendered_template_path="${template/$templates_source\//}"
+  rendered_output_file="$rendered_output/$rendered_template_path"
+  mkdir -p "$(dirname "$rendered_output_file")"
+  render-template "$template" > "$rendered_output_file"
+done
